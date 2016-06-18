49e4e5d860259669e9dd63af9154f408c6b88166
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-06-19 01:29:01 +0700

    convert: add source

---
+++ modules/archive/views/o/convert/admin_add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Archive Converts (archive-converts)
+ * @var $this ConvertController
+ * @var $model ArchiveConverts
+ * @var $form CActiveForm
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 19 June 2016, 01:23 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+
+	$this->breadcrumbs=array(
+		'Archive Converts'=>array('manage'),
+		'Create',
+	);
+?>
+
+<div class="form">
+	<?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+</div>
