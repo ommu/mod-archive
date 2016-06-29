df9111197961193689eb6b62c830ff5f2c25c8ad
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-06-29 13:33:42 +0700

    modify copyright

--- modules/archive/views/o/convert/admin_manage.php
+++ modules/archive/views/o/convert/admin_manage.php
@@ -5,7 +5,7 @@
  * @var $model ArchiveConverts
  * version: 0.0.1
  *
- * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
  * @created date 19 June 2016, 01:23 WIB
  * @link http://company.ommu.co
