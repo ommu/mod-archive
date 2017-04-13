2b0098f2993218fceb8e09b80771feeb09973e8d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-13 15:34:12 +0700

    modify copyright information

--- modules/archive/models/ViewArchiveType.php
+++ modules/archive/models/ViewArchiveType.php
@@ -4,7 +4,7 @@
  * version: 0.0.1
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
- * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @copyright Copyright (c) 2016 Ommu Platform (opensource.ommu.co)
  * @created date 16 June 2016, 13:44 WIB
  * @link http://company.ommu.co
  * @contact (+62)856-299-4114
