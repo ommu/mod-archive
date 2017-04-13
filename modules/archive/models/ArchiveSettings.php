a3e6206f09806f6d6f5fc0c2482ca3d4158d8079
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-13 16:10:15 +0700

    commit

--- modules/archive/models/ArchiveSettings.php
+++ modules/archive/models/ArchiveSettings.php
@@ -231,7 +231,7 @@ class ArchiveSettings extends CActiveRecord
 					'model'=>$this,
 					'attribute'=>'modified_date',
 					'language' => 'en',
-					'i18nScriptFile' => 'jquery-ui-i18n.min.js'
+					'i18nScriptFile' => 'jquery-ui-i18n.min.js',
 					//'mode'=>'datetime',
 					'htmlOptions' => array(
 						'id' => 'modified_date_filter',
