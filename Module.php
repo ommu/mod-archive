4965a3cea17063929b958dd9847a7269325501d7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-05 23:58:49 +0700

    add module structure

---
+++ Module.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * archive module definition class
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 5 March 2019, 23:14 WIB
+ * @link https://bitbucket.org/ommu/archive
+ *
+ */
+
+namespace ommu\archive;
+
+class Module extends \app\components\Module
+{
+	public $layout = 'main';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public $controllerNamespace = 'ommu\archive\controllers';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function init()
+	{
+		parent::init();
+
+		// custom initialization code goes here
+	}
+}
---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * archive module config
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2019 OMMU (www.ommu.co)
+ * @created date 5 March 2019, 23:14 WIB
+ * @link https://bitbucket.org/ommu/archive
+ *
+ */
+
+return [
+	'id' => 'archive',
+	'class' => ommu\archive\Module::className(),
+];
