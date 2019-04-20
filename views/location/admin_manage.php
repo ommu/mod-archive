5b8d14d291d33c43b2444cfda4114be6b89c2c36
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-04-20 12:44:07 +0700

    drop url merge in admin_manage

--- views/location/admin_manage.php
+++ views/location/admin_manage.php
@@ -18,7 +18,6 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use app\components\widgets\GridView;
 use yii\widgets\Pjax;
-use yii\helpers\ArrayHelper;
 
 $this->params['breadcrumbs'][] = $this->title;
 
@@ -49,12 +48,12 @@ array_push($columnData, [
 	'buttons' => [
 		'view' => function ($url, $model, $key) {
 			$context = $this->context;
-			$url = Url::to(ArrayHelper::merge(['view', 'id'=>$model->primaryKey], Yii::$app->request->get()));
+			$url = Url::to(['view', 'id'=>$model->primaryKey]);
 			return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, ['title' => Yii::t('app', 'Detail {title}', ['title'=>Yii::t('app', $this->context->title)])]);
 		},
 		'update' => function ($url, $model, $key) {
 			$context = $this->context;
-			$url = Url::to(ArrayHelper::merge(['update', 'id'=>$model->primaryKey], Yii::$app->request->get()));
+			$url = Url::to(['update', 'id'=>$model->primaryKey]);
 			return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, ['title' => Yii::t('app', 'Update {title}', ['title'=>Yii::t('app', $this->context->title)])]);
 		},
 		'delete' => function ($url, $model, $key) {
