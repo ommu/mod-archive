8e081274a4110b4ea35aac92f996713401fda23e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2021-08-25 06:58:44 +0700

    migration: add insert role and menu

---
+++ migrations/m210825_063233_archive_module_insert_role.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * m210825_063233_archive_module_insert_role
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2020 OMMU (www.ommu.id)
+ * @created date 25 August 2021, 06:33 WIB
+ * @link https://bitbucket.org/ommu/archive
+ *
+ */
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\rbac\DbManager;
+
+class m210825_063233_archive_module_insert_role extends \yii\db\Migration
+{
+    /**
+     * @throws yii\base\InvalidConfigException
+     * @return DbManager
+     */
+    protected function getAuthManager()
+    {
+        $authManager = Yii::$app->getAuthManager();
+        if (!$authManager instanceof DbManager) {
+            throw new InvalidConfigException('You should configure "authManager" component to use database before executing this migration.');
+        }
+
+        return $authManager;
+    }
+
+	public function up()
+	{
+        $authManager = $this->getAuthManager();
+        $this->db = $authManager->db;
+        $schema = $this->db->getSchema()->defaultSchema;
+
+		$tableName = Yii::$app->db->tablePrefix . $authManager->itemTable;
+        if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert($tableName, ['name', 'type', 'data', 'created_at'], [
+				['archiveModLevelAdmin', '2', '', time()],
+				['archiveModLevelModerator', '2', '', time()],
+				['/archive/admin/*', '2', '', time()],
+				['/archive/admin/index', '2', '', time()],
+				['/archive/fond/*', '2', '', time()],
+				['/archive/fond/index', '2', '', time()],
+				['/archive/setting/admin/delete', '2', '', time()],
+				['/archive/setting/admin/index', '2', '', time()],
+				['/archive/setting/admin/update', '2', '', time()],
+				['/archive/setting/creator/*', '2', '', time()],
+				['/archive/setting/level/*', '2', '', time()],
+				['/archive/setting/media/*', '2', '', time()],
+				['/archive/setting/repository/*', '2', '', time()],
+				['/archive/view/admin/*', '2', '', time()],
+				['/archive/view/history/*', '2', '', time()],
+			]);
+		}
+
+		$tableName = Yii::$app->db->tablePrefix . $authManager->itemChildTable;
+        if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert($tableName, ['parent', 'child'], [
+				['userAdmin', 'archiveModLevelAdmin'],
+				['userModerator', 'archiveModLevelModerator'],
+				['archiveModLevelAdmin', 'archiveModLevelModerator'],
+				['archiveModLevelAdmin', '/archive/setting/admin/delete'],
+				['archiveModLevelAdmin', '/archive/setting/admin/update'],
+				['archiveModLevelAdmin', '/archive/setting/level/*'],
+				['archiveModLevelModerator', '/archive/admin/*'],
+				['archiveModLevelModerator', '/archive/fond/*'],
+				['archiveModLevelModerator', '/archive/setting/admin/index'],
+				['archiveModLevelModerator', '/archive/setting/creator/*'],
+				['archiveModLevelModerator', '/archive/setting/media/*'],
+				['archiveModLevelModerator', '/archive/setting/repository/*'],
+				['archiveModLevelModerator', '/archive/view/admin/*'],
+				['archiveModLevelModerator', '/archive/view/history/*'],
+			]);
+		}
+	}
+}
---
+++ migrations/m210825_064935_archive_module_insert_menu.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * m210825_064935_archive_module_insert_menu
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2020 OMMU (www.ommu.id)
+ * @created date 25 August 2020, 06:50 WIB
+ * @link https://bitbucket.org/ommu/archive
+ *
+ */
+
+use Yii;
+use mdm\admin\components\Configs;
+use app\models\Menu;
+
+class m210825_064935_archive_module_insert_menu extends \yii\db\Migration
+{
+	public function up()
+	{
+        $menuTable = Configs::instance()->menuTable;
+		$tableName = Yii::$app->db->tablePrefix . $menuTable;
+
+        if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert($tableName, ['name', 'module', 'icon', 'parent', 'route', 'order', 'data'], [
+				['SIKS', 'archive', 'fa-archive', null, '/#', null, null],
+			]);
+		}
+
+        if (Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->batchInsert($tableName, ['name', 'module', 'icon', 'parent', 'route', 'order', 'data'], [
+				['Inventory', 'archive', null, Menu::getParentId('SIKS#archive'), '/archive/admin/index', null, null],
+				['Settings', 'archive', null, Menu::getParentId('SIKS#archive'), '/archive/setting/admin/index', null, null],
+				['Fonds', 'archive', null, Menu::getParentId('SIKS#archive'), '/archive/fond/index', null, null],
+			]);
+		}
+	}
+}
