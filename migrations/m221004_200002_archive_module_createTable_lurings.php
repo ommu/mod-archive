5cd2b3362f0b0f4a67ea70a83042758174808e6b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-10-04 20:17:18 +0700

    migrate: createTable

---
+++ migrations/m221004_200002_archive_module_createTable_lurings.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * m221004_200002_archive_module_createTable_lurings
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 4 October 2022, 20:01 WIB
+ * @link https://bitbucket.org/ommu/archive
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m221004_200002_archive_module_createTable_lurings extends \yii\db\Migration
+{
+	public function up()
+	{
+		$tableOptions = null;
+		if ($this->db->driverName === 'mysql') {
+			$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+		}
+		$tableName = Yii::$app->db->tablePrefix . '{{%ommu_archive_lurings}}';
+		if (!Yii::$app->db->getTableSchema($tableName, true)) {
+			$this->createTable($tableName, [
+				'id' => Schema::TYPE_INTEGER . '(11) UNSIGNED NOT NULL AUTO_INCREMENT',
+				'publish' => Schema::TYPE_TINYINT . '(1) NOT NULL DEFAULT \'1\'',
+				'archive_id' => Schema::TYPE_INTEGER . '(11) UNSIGNED',
+				'introduction' => Schema::TYPE_TEXT . ' NOT NULL',
+				'senarai_file' => Schema::TYPE_TEXT . ' NOT NULL',
+				'creation_date' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT \'trigger\'',
+				'creation_id' => Schema::TYPE_INTEGER . '(11) UNSIGNED',
+				'modified_date' => Schema::TYPE_TIMESTAMP . ' NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT \'trigger,on_update\'',
+				'modified_id' => Schema::TYPE_INTEGER . '(11) UNSIGNED',
+				'updated_date' => Schema::TYPE_DATETIME . ' NOT NULL DEFAULT \'0000-00-00 00:00:00\' COMMENT \'trigger\'',
+				'PRIMARY KEY ([[id]])',
+				'CONSTRAINT ommu_archive_lurings_ibfk_1 FOREIGN KEY ([[archive_id]]) REFERENCES {{%ommu_archives}} ([[id]]) ON DELETE CASCADE ON UPDATE CASCADE',
+			], $tableOptions);
+
+			$this->createIndex(
+				'publicWithArchive',
+				$tableName,
+				['publish', 'archive_id']
+			);
+		}
+	}
+
+	public function down()
+	{
+		$tableName = Yii::$app->db->tablePrefix . '{{%ommu_archive_lurings}}';
+		$this->dropTable($tableName);
+	}
+}
